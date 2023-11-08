44d77c83fd3a3401e6db77f27522eaa89e854756
Muhammad Muhyillail <56330224+Liyhum@users.noreply.github.com>
2023-11-08 23:15:20 +0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let birthday = "October 30, 2023 00:00:00",
+    let birthday = "November 08, 2023 00:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {
             let now = new Date().getTime(),
